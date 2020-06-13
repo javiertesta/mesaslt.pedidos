8fade44796c88ca037fbcf511be9a49705ea6f91
Javier Testa <jtsoft@live.com.ar>
2020-06-13 00:28:30 -0300

    Actualización de paquetes NuGet

--- Pedidos/Scripts/_references.js
+++ Pedidos/Scripts/_references.js
@@ -1,4 +1,4 @@
-﻿/// <reference path="jquery-3.3.1.js" />
+﻿/// <reference path="jquery-3.5.1.js" />
 /// <reference path="jquery-ui-1.12.1.js" />
 /// <autosync enabled="true" />
 /// <reference path="jquery.validate.unobtrusive.js" />
